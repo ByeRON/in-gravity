61c186fbed18b61f73d16baed74f351d7be40c11
byeron <disce.uno@gmail.com>
2021-05-03 12:30:27 +0900

    delete extra space in json schema

--- ui/restapi/user/create/defs.go
+++ ui/restapi/user/create/defs.go
@@ -1,16 +1,11 @@
 package create
 
-<<<<<<< HEAD
+import "in-gravity/ui/restapi/defs"
+
 type RequestCreateUser struct {
-	Name UserName
+	Name defs.UserName `json:"Name" validate:"required" minLength:"3" example:"aiue ou"`
 }
 
 type ResponseCreateUser struct {
 	UserId string
-=======
-import "in-gravity/ui/restapi/defs"
-
-type RequestCreateUser struct {
-	Name defs.UserName `json:"Name" validate:"required" minLength:"3" example: "aiue ou"`
->>>>>>> Add UserName def
 }
