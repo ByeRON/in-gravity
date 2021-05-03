30ab8b6fa8291f68f1fdb8f4ad2718d32b320d0d
byeron <disce.uno@gmail.com>
2021-05-03 13:57:01 +0900

    Add api def

--- ui/restapi/route.go
+++ ui/restapi/route.go
@@ -4,14 +4,28 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	httpSwagger "github.com/swaggo/http-swagger"
 
 	"in-gravity/config"
+	_ "in-gravity/docs"
 	createUser "in-gravity/ui/restapi/user/create"
 	"in-gravity/x/ui/restapi/handler"
 )
 
+// @title In-Gravity API
+// @version 1.0
+// @description This is a great application
+
+// @host localhost:3000
+// @BasePath /
+// @query.collection.format multi
 func Serve(conf config.ApplicationConfig) {
 	r := chi.NewRouter()
+
+	r.Get("/swagger/*", httpSwagger.Handler(
+		httpSwagger.URL("/swagger/doc.json"),
+	))
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
